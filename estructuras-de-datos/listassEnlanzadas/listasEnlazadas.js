cb9de15816eb2f9ed8f41fec36a9aace0786c0be
dan123va <42777196+dan123va@users.noreply.github.com>
2023-07-25 20:30:42 -0600

    Se agreego el codigo de una lista enlazada, el archivo de for y while y el tema de paso por valor y por referencia

---
+++ estructuras-de-datos/listassEnlanzadas/listasEnlazadas.js
@@ -0,0 +1,33 @@
+const list = {
+
+  head: {
+
+    value: 6,
+    next: {
+
+      value: 10,
+      next: {
+
+        value: 12,
+        next: {
+
+          value: 3,
+          next: null,
+
+        },
+
+      },
+
+    },
+
+  },
+
+};
+
+const listNull = {
+
+  head: null
+
+};
+
+head = new Node('1', 'next')
